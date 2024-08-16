e262c969ea697e1bd9f765780a4f13b18f230cc4
Ambuj00 <ambuj@valuebound.com>
2024-08-16 17:34:50 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,62 @@
+import requests
+import streamlit as st
+
+def get_groq_response(input_text, language):
+    json_body = {
+        "input": {
+            "language": language,
+            "text": f"{input_text}"
+        },
+        "config": {},
+        "kwargs": {}
+    }
+    # Use your Render URL here
+    response = requests.post("https://ex-24-llm-application.onrender.com/chain/invoke", json=json_body)
+
+    response_json = response.json()
+    output = response_json.get("output", "No output received")  # Handle case where output might not be present
+
+    return output
+
+## Streamlit app
+st.title("LLM Application Using LCEL")
+
+# Dropdown for language selection
+language_options = [
+   "Portuguese",
+   "Spanish",
+   "German",
+   "French",
+   "Italian",
+   "Dutch",
+   "Russian",
+   "Japanese",
+   "Chinese",
+   "Korean",
+   "Hindi",
+   "Urdu",
+   "Tamil",
+   "Telugu",
+   "Odia"
+]  # Add more languages as needed
+selected_language = st.selectbox("Select language", language_options)
+
+input_text = st.text_area("Enter the text you want to translate", height=150)
+
+if input_text:
+    with st.spinner("Translating..."):
+        translated_text = get_groq_response(input_text, selected_language)
+        
+    # Display the translated text
+    st.subheader("Translated Text:")
+    st.markdown(f"""
+    <div style="
+        padding: 10px;
+        border: 2px solid #4CAF50;
+        border-radius: 5px;
+        background-color: #f9f9f9;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    ">
+        <p style="font-size: 18px; color: #333;">{translated_text}</p>
+    </div>
+    """, unsafe_allow_html=True)
---
+++ serve.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+import os
+from langserve import add_routes
+from dotenv import load_dotenv
+load_dotenv()
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
+groq_api_key=os.getenv("GROQ_API_KEY")
+model=ChatGroq(model="Gemma2-9b-It",groq_api_key=groq_api_key)
+
+# 1. Create prompt template
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+
+parser=StrOutputParser()
+
+##create chain
+chain=prompt_template|model|parser
+
+
+
+## App definition
+app=FastAPI(title="Langchain Server",
+            version="1.0",
+            description="A simple API server using Langchain runnable interfaces")
+
+# Add CORS middleware to your FastAPI app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://ex24llmapplication-k8az6hqp9pf8gtopa6vbm4.streamlit.app"],  # Replace with your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+## Adding chain routes
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app,host="127.0.0.1",port=8000)
+
+
